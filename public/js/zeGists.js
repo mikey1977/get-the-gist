4bc9d3b18df669c6f0949dc980c1ecdd5657697d
BraddahBoots <bryan@thebrocode.io>
2015-11-03 11:36:35 -1000

    established routes on index template

--- public/js/zeGists.js
+++ public/js/zeGists.js
@@ -1,10 +1,37 @@
-angular.module('zeGists', []);
+angular.module('zeGists', ['ngRoute']);
 
 var zeGists = angular.module('zeGists');
 
 zeGists
-.config(function() {
+.config(function($routeProvider) {
 
+  //enable logic to check if current user has auth-token stored in local storage
+
+  //route architicture for zeGists
+  $routeProvider
+  //user directed to login page when no token in local storage
+  .when('/login', {
+    templateUrl : 'views/login.html',
+    controller : 'loginController',
+  })
+
+  //user directed to gists page when token in local storage
+  .when('/gists', {
+    templateUrl : 'views/gists.html',
+    controller : 'myController'
+  })
+
+  //user directed to gists:id when selecting specific gists to work on
+  .when('/gists:id', {
+    templateUrl : 'views/gists-id.html'
+    // controller :
+  })
+
+  //when user breaks things
+  .otherwise({
+    templateUrl : 'views/404.html'
+    // controller :
+  });
 })
 .run(['$rootScope', 'APP_VERSION', function($rootScope, APP_VERSION) {
 
