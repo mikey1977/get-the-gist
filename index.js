c87d472c7e2b5b1822d222fde5044d85617c71ca
mikey1977 <mmi@umich.edu>
2015-11-02 15:57:19 -1000

    added api route

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-"use strict";
+'use strict';
 const PORT = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+var api = require('/public/route/api');
 var OAuth2 = require('oauth').OAuth2;
 var oauth = new OAuth2(
   process.env.GITHUB_CLIENT_ID,
@@ -20,7 +21,8 @@ app.use(express.static('./public'));
 //parse json encoded callback through middleware
 app.use(bodyParser.urlencoded({ extended : true }));
 
-
+//route to the api router '/public/route/api'
+app.use('/api', api);
 
 app.listen(PORT, function() {
   console.log('# Butt');
