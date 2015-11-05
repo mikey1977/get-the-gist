c9ad5c681142c568dbd6ea6ab3d01a2c9655c212
mikey1977 <mmi@umich.edu>
2015-11-04 19:35:23 -1000

    redirect to home after submitting new gist

--- public/js/controllers/newGist-controller.js
+++ public/js/controllers/newGist-controller.js
@@ -1,10 +1,11 @@
 var zeGists = angular.module('zeGists')
-.controller('newGistController', ['$scope', 'httpRequest', function($scope, httpRequest) {
+.controller('newGistController', ['$scope', '$location', 'httpRequest', function($scope, $location, httpRequest) {
   $scope.create = function(newGist) {
     console.log('new Gist', newGist);
     httpRequest.newGists(newGist)
     .success(function(newGist) {
       console.log('callback', newGist);
+      $location.path('/gists');
     });
   };
 }]);
